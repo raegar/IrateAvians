eb7283f26024262f7f430084b085b6757dd2f851
Jamie Myland <jamie.myland@gmail.com>
2022-11-15 22:58:14 +0000

    Made the launcher object rotate to point at mouse

--- Assets/Scripts/LauncherController.cs
+++ Assets/Scripts/LauncherController.cs
@@ -4,15 +4,28 @@ using UnityEngine;
 
 public class LauncherController : MonoBehaviour
 {
+    public Camera MainCamera;
+    public GameObject LauncherBase;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vector2 mousePosition = Input.mousePosition;
+        Vector3 worldPosition = MainCamera.ScreenToWorldPoint(new Vector3(mousePosition.x, mousePosition.y, transform.position.z - MainCamera.transform.position.z));
+
+        if (worldPosition.x > LauncherBase.transform.position.x + 1) //Only when the mouse is to the Right of the Launcher
+        {
+            LauncherBase.transform.localEulerAngles = new Vector3(
+                LauncherBase.transform.localEulerAngles.x,
+                LauncherBase.transform.localEulerAngles.y,
+                Mathf.Atan2((worldPosition.y - LauncherBase.transform.position.y), (worldPosition.x - LauncherBase.transform.position.x)) * Mathf.Rad2Deg
+            );
+        }
     }
 }
